c8442b4e1d4a594d9053b6e4c75049a770eaa4e0
fabien.daoulas <fabien.daoulas@alten.nl>
2016-03-09 09:54:12 +0100

    several bu fix, everything working well now

--- server/server.js
+++ server/server.js
@@ -5,21 +5,15 @@ var zmq = require('zmq');
 var proxyPullSocket = zmq.socket('pull');
 
 proxyPullSocket.on('message', (message) => {
-    console.info('[message]', message);
-    
     var json = JSON.parse(message);
+    console.info('[message]', json);
     
-    if(json.client) {
+    if(json.client !== undefined) {
         var filePath = './clientLog/client' + json.client + '.txt';
-        fs.access(filePath, (err, stats) => {
-            if(err.code == 'ENOENT') {
-                console.info('[info]', 'no such file. Create one...');
-                fs.writeFile(filePath, json.data, (err) => {
-                    if(err) throw err;
-                    console.info('[info]', 'Client', json.client, 'Message saved.');
-                }); 
-            }
-        });
+        fs.writeFile(filePath, json.data, (err) => {
+            if(err) throw err;
+            console.info('[info]', 'Client', json.client, 'Message saved.');
+        }); 
     }
 });
 
